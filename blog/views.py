aa09c7b40cdc61d3244c8b7caa4dd6ba4d29a513
Мартов Олексійович <vadym.martov@nure.ua>
2023-08-09 21:03:27 +0000

    Caching (for test on local machine)

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,21 @@ import logging
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie, vary_on_headers
 from blog.forms import CommentForm
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+@cache_page(300)
+@vary_on_cookie
 def index(request):
+  from django.http import HttpResponse
+  logger.debug("Index function is called!")
+  return HttpResponse(str(request.user).encode("ascii"))
   posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug(f"Got %d posts", len(posts))
-  logger.info("User %s checked the main page", request.user)
   return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
