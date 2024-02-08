29b159f6a52f804843d0898a87856d8ace5f548f
Вадим Мартов <vadym.martov@nure.ua>
2024-02-08 11:29:39 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,7 +1,21 @@
-const someNumber = 2;
-
-if (someNumber === 1) {
-  let name = "Olovo";
-  alert(name);
+for (let i = 0; i < 10; i++) {
+  console.log("For-loop counter: " + i);
 }
-alert(name);
+
+let j = 0;
+while (j < 10) {
+  console.log("While-loop j: " + j);
+  j += 1;
+}
+
+let k = 10;
+
+do {
+  console.log("do-while loop k: " + k);
+} while (k < 10);
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach((value) => {console.log("Element: " + value)});
+const squared = numbers.map(value => value ** 2);
+console.log(squared);
