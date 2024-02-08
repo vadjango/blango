84020d2d4e90249fda47ae10bd1535f21e7443e9
Вадим Мартов <vadym.martov@nure.ua>
2024-02-08 10:40:16 +0000

    Finish JavaScript intro and fundamentals

---
+++ blog/static/blog/blog.js
@@ -0,0 +1,7 @@
+const someNumber = 2;
+
+if (someNumber === 1) {
+  let name = "Olovo";
+  alert(name);
+}
+alert(name);
--- blog/views.py
+++ blog/views.py
@@ -40,3 +40,6 @@ def post_detail(request, slug):
         comment_form = None
     return render(request, "blog/post-detail.html", {"post": post,
                                                    "comment_form": comment_form})
+
+def post_table(request):
+  return render(request, "blog/post-table.html")
